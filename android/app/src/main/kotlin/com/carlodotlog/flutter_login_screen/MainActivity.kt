89a8c105adab506df5d2590c73cc0618cbfdb533
carlo.loguercio <carlo.loguercio@accenture.com>
2022-04-15 01:24:11 +0200

    Setup local_auth on Android

--- android/app/src/main/kotlin/com/carlodotlog/flutter_login_screen/MainActivity.kt
+++ android/app/src/main/kotlin/com/carlodotlog/flutter_login_screen/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.carlodotlog.flutter_login_screen
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
